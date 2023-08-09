48159538aaf019c77ecb26163ec66d917e59eff0
Clear312 <clearance.boateng@stud.leuphana.de>
2023-08-09 22:20:42 +0200

    Update index.js

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,7 +1,7 @@
 const data = require('../shared/product-data');
 
 module.exports = async function (context, req) {
-  // Test
+  // Test123
   try {
     const products = data.getProducts();
     context.res.status(200).json(products);
